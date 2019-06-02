da3894952fdcbd364a86ea1279cbd9164412781a
Piyush <bhangalepiyush@gmail.com>
2019-06-02 19:07:35 +0530

    Formatting v1.1

--- jcb.py
+++ jcb.py
@@ -5,27 +5,33 @@ from discord.ext import commands
 with open("./config.json") as f:
     config = json.load(f)
 
-client = commands.Bot(command_prefix=config["prefix"], description="Reacts With a JCB Emoji to the message")
+client = commands.Bot(
+    command_prefix=config["prefix"],
+    description="Reacts With a JCB Emoji to the message",
+)
 
 
 @client.event
 async def on_ready():
-    print("     _  _____ ____  "
-          "\n| |/ ____|  _ \ "
-          "\n| | |    | |_) |"
-          "\n| | |    |  _ < "
-          "\n| |__| | |____| |_) |"
-          "\n \____/ \_____|____/ ")
-    print(f'Successfully logged in and booted...!')
+    print(
+        "     _  _____ ____  "
+        "\n| |/ ____|  _ \ "
+        "\n| | |    | |_) |"
+        "\n| | |    |  _ < "
+        "\n| |__| | |____| |_) |"
+        "\n \____/ \_____|____/ "
+    )
+    print(f"Successfully logged in and booted...!")
 
 
 @client.event()
 async def on_message(msg: discord.Message):
-    if msg.channel.id !=  config["channel"]:
+    if msg.channel.id != config["channel"]:
         return
     else:
         emoji: discord.Emoji = await msg.guild.fetch_emoji(int(config["emote"]))
         await msg.add_reaction(emoji=emoji)
         return
+
 
 client.run(config["token"])
